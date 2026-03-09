37651b42fd4eb424cbcbc9e85bf561ed5c62ee70
N1vn0vem8er <fq0xveupd5d@outlook.com>
2026-03-09 15:11:21 +0100

    Added navbar and main page layout

---
+++ ATTRIBUTIONS.md
@@ -0,0 +1,9 @@
+Sqlite-square-icon.svg
+Source: https://commons.wikimedia.org/wiki/File:Sqlite-square-icon.svg
+
+"SQLite370.svg: Part of the SQLite documentation, which has been released by
+author D. Richard Hipp to the public domain. (SVG version: Mike Toews)
+derivative work: Loki 66"
+
+License: CC BY-SA 3.0
+https://creativecommons.org/licenses/by-sa/3.0/
---
+++ scripts/navbar.js
@@ -0,0 +1,52 @@
+class MainNavbar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <nav>
+  <div class="wrapper">
+    <div class="logo"><a href="#">N1vn0vem8er</a></div>
+    <input type="radio" name="slider" id="menu-btn">
+    <input type="radio" name="slider" id="close-btn">
+    <ul class="nav-links">
+      <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li>
+        <a href="#" class="desktop-item">Projects</a>
+        <input type="checkbox" id="showMega">
+        <label for="showMega" class="mobile-item">Projects</label>
+        <div class="mega-box">
+          <div class="content">
+            <div class="row">
+              <header>Developer tools</header>
+              <ul class="mega-links">
+                <li><a href="#">8080 IDE</a></li>
+                <li><a href="#">Cute Toolbox</a></li>
+                <li><a href="#">CppEasyRun</a></li>
+              </ul>
+            </div>
+            <div class="row">
+              <header>Desktop applications</header>
+              <ul class="mega-links">
+                <li><a href="#">Pinecone Snake</a></li>
+                <li><a href="#">Recipsimplgo</a></li>
+              </ul>
+            </div>
+            <div class="row">
+              <header>Other</header>
+              <ul class="mega-links">
+                <li><a href="#">Very Simple Power Menu</a></li>
+                <li><a href="#">Analog Clock Widget</a></li>
+                <li><a href="#">Usb data sender</a></li>
+                <li><a href="#"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+  </div>
+</nav>`;
+  }
+}
+customElements.define('main-navbar', MainNavbar);
